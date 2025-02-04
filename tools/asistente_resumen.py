72db085ff0ab3dd961d33a5713a22bc31332a61c
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2025-02-04 13:37:55 +0100

    fix

--- tools/asistente_resumen.py
+++ tools/asistente_resumen.py
@@ -10,20 +10,20 @@ st.markdown("### Asistente de Resumen")
 # Inicializar session_state si no existe
 if "historial_conversacion" not in st.session_state:
     st.session_state.historial_conversacion = []
-if "input_key" not in st.session_state:
-    st.session_state.input_key = 0  # Clave dinámica para resetear input
+if "input_key_resumen" not in st.session_state:
+    st.session_state.input_key_resumen = 0  # Clave dinámica para resetear input
 if "thread_conversacion" not in st.session_state:
     st.session_state.thread_conversacion = ''  # Clave dinámica para resetear input
-if 'button_ventas' not in st.session_state:
-    st.session_state.button_ventas = False
+if 'button_resumen' not in st.session_state:
+    st.session_state.button_resumen = False
 
 def click_button_send():
-    st.session_state.button_ventas = not st.session_state.button_ventas
+    st.session_state.button_resumen = not st.session_state.button_resumen
 
 st.text_area("Historico de la conversacion:", value="\n".join(st.session_state.historial_conversacion), height=350, disabled=True)
 
 # Entrada de texto con clave dinámica
-user_input = st.text_input("Introduce la conversacion para proceder al analisis:", key=f"user_input_{st.session_state.input_key}")
+user_input = st.text_input("Introduce la conversacion para proceder al analisis:", key=f"user_input_{st.session_state.input_key_resumen}")
 
 id_asis = st.secrets['security']['OPENAI_API_ASSIS_CONVERSACION']
 
@@ -34,15 +34,15 @@ st.session_state.thread_conversacion = sp.create_thread(openai_client)
 col1, col2, col3 , col4, col5, col6, col7, col8 , col9, col10 = st.columns(10)
 
 with col1:
-    if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_ventas):
+    if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_resumen):
         if user_input.strip():  # Evita entradas vacías
             respuesta = sp.process_data(openai_client, assis_venta.id, st.session_state.thread_conversacion.id, user_input)
             st.session_state.historial_conversacion.append(f"Tú: {user_input}" + '\n' + 'Asistente: '+ respuesta)
-            st.session_state.input_key += 1  # Cambiar clave para limpiar input
-            st.session_state.button_ventas = False
+            st.session_state.input_key_resumen += 1  # Cambiar clave para limpiar input
+            st.session_state.button_resumen = False
             st.rerun()  # Refrescar la app para borrar input
         else:
-            st.session_state.button_ventas = False
+            st.session_state.button_resumen = False
             st.rerun()  # Refrescar la app para borrar input
 with col2:
     pass
