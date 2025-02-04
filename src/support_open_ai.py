fb78f6651950fe5968ce2d970ab14b3f2dc1bb55
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2025-02-04 12:59:15 +0100

    asistente ventas

---
+++ src/support_open_ai.py
@@ -0,0 +1,166 @@
+# Importaciones
+import pandas as pd # type: ignore
+import time
+import sys
+sys.path.append("../")
+
+import os
+import streamlit as st # type: ignore
+from openai import OpenAI # type: ignore
+
+def init_openai():
+    """
+    Inicializa el cliente de OpenAI utilizando la clave API almacenada en las variables de entorno.
+
+    Returns:
+        OpenAI: Instancia del cliente de OpenAI.
+    """
+    OPENAI_API_KEY = st.secrets['security']['OPENAI_API_KEY']
+    return OpenAI(api_key=OPENAI_API_KEY)
+
+def get_assistant(openai_client, assistant_id):
+    """
+    Recupera un asistente específico de OpenAI a partir de su ID.
+
+    Args:
+        openai_client (OpenAI): Cliente de OpenAI.
+        assistant_id (str): ID del asistente a recuperar.
+
+    Returns:
+        dict: Datos del asistente recuperado.
+    """
+    return openai_client.beta.assistants.retrieve(
+        assistant_id=assistant_id
+    )
+
+def create_thread(openai_client):
+    """
+    Crea un nuevo hilo de conversación en OpenAI.
+
+    Args:
+        openai_client (OpenAI): Cliente de OpenAI.
+
+    Returns:
+        dict: Información del hilo creado.
+    """
+    return openai_client.beta.threads.create()
+
+def process_data(openai_client, assistant_id, thread_id, message):
+    """
+    Envía un mensaje a un asistente de OpenAI y procesa su respuesta.
+
+    Args:
+        openai_client (OpenAI): Cliente de OpenAI.
+        assistant_id (str): ID del asistente de OpenAI.
+        thread_id (str): ID del hilo en el que se enviará el mensaje.
+        message (str): Mensaje a enviar al asistente.
+
+    Returns:
+        str: Respuesta del asistente si está disponible, de lo contrario, None.
+    """
+    openai_client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message,
+    )
+
+    run = openai_client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id
+    )
+
+    run_status = openai_client.beta.threads.runs.retrieve(
+        thread_id=thread_id,
+        run_id=run.id
+    )
+
+    while True:
+        run_status = openai_client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+        if run_status.status == "completed":
+            print("se completó exitosamente.")
+            break
+        elif run_status.status == "failed":
+            print("petó.")
+            break
+        else:
+            print(f"Esperando a que se complete...{run.id}")
+            time.sleep(3)
+
+    print(f"Thread ID: {thread_id}")
+    print(f"Run ID: {run.id}")
+    print(f"Run Status: {run_status.status}")
+
+    response_messages = openai_client.beta.threads.messages.list(thread_id=thread_id)
+
+    assistant_response = None
+    for message in response_messages.data:
+            assistant_response = "\n".join([block.text.value for block in message.content])
+            break
+
+    if assistant_response:
+        print(f"respuesta procesada del asistente.")
+    else:
+        print("No se encontró una respuesta del asistente.")
+
+    return assistant_response
+
+def process_data_sin_mensajes(openai_client, assistant_id, thread_id, message):
+    """
+    Envía un mensaje a un asistente de OpenAI y obtiene la respuesta sin imprimir mensajes intermedios.
+
+    Args:
+        openai_client (OpenAI): Cliente de OpenAI.
+        assistant_id (str): ID del asistente de OpenAI.
+        thread_id (str): ID del hilo en el que se enviará el mensaje.
+        message (str): Mensaje a enviar al asistente.
+
+    Returns:
+        str: Respuesta del asistente si está disponible, o un mensaje de error si la solicitud falla.
+    """
+
+    assistant_response = "No se encontró una respuesta del asistente."
+
+    openai_client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message,
+    )
+
+    run = openai_client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id
+    )
+
+    run_status = openai_client.beta.threads.runs.retrieve(
+        thread_id=thread_id,
+        run_id=run.id
+    )
+
+    while True:
+        run_status = openai_client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+        if run_status.status == "completed":
+            break
+        elif run_status.status == "failed":
+            print("Error, no se encontró una respuesta del asistente. Fallo en llamada a OpenAI.")
+            return assistant_response
+        else:
+            time.sleep(3)
+
+    response_messages = openai_client.beta.threads.messages.list(thread_id=thread_id)
+
+    
+    for message in response_messages.data:
+            assistant_response = "\n".join([block.text.value for block in message.content])
+            break
+
+   
+    return assistant_response
+
+
+
--- tools/asistente_ventas.py
+++ tools/asistente_ventas.py
@@ -1,5 +1,9 @@
 import streamlit as st # type: ignore
 
+import sys
+sys.path.append("../")
+from src import support_open_ai as sp 
+
 # Título de la aplicación
 st.markdown("### Asistente de Ventas")
 
@@ -8,6 +12,8 @@ if "historial_ventas" not in st.session_state:
     st.session_state.historial_ventas = []
 if "input_key" not in st.session_state:
     st.session_state.input_key = 0  # Clave dinámica para resetear input
+if "thread_ventas" not in st.session_state:
+    st.session_state.thread_ventas = ''  # Clave dinámica para resetear input
 
 
 st.text_area("Respuesta del asistente:", value="\n".join(st.session_state.historial_ventas), height=350, disabled=True)
@@ -15,12 +21,19 @@ st.text_area("Respuesta del asistente:", value="\n".join(st.session_state.histor
 # Entrada de texto con clave dinámica
 user_input = st.text_input("Pregunta sobre el curso de Full Stack:", key=f"user_input_{st.session_state.input_key}")
 
+id_asis = st.secrets['security']['OPENAI_API_ASSIS_VENTA']
+
+openai_client = sp.init_openai()
+assis_venta = sp.get_assistant(openai_client, id_asis)
+st.session_state.thread_ventas = sp.create_thread(openai_client)
+
 col1, col2, col3 , col4, col5, col6, col7, col8 , col9, col10 = st.columns(10)
 
 with col1:
     if st.button("Enviar"):
         if user_input.strip():  # Evita entradas vacías
-            st.session_state.historial_ventas.append(f"Tú: {user_input}")
+            respuesta = sp.process_data(openai_client, assis_venta.id, st.session_state.thread_ventas.id, user_input)
+            st.session_state.historial_ventas.append(f"Tú: {user_input}" + '\n' + 'Asistente: '+ respuesta)
             st.session_state.input_key += 1  # Cambiar clave para limpiar input
             st.rerun()  # Refrescar la app para borrar input
 with col2:
