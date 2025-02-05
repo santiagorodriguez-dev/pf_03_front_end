82c8986ec2f11f1acce3ea7cf5528758a298b390
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2025-02-05 12:16:32 +0100

    fix

--- tools/asistente_resumen.py
+++ tools/asistente_resumen.py
@@ -12,8 +12,6 @@ if "historial_conversacion" not in st.session_state:
     st.session_state.historial_conversacion = []
 if "input_key_resumen" not in st.session_state:
     st.session_state.input_key_resumen = 0  # Clave dinámica para resetear input
-if "thread_conversacion" not in st.session_state:
-    st.session_state.thread_conversacion = ''  # Clave dinámica para resetear input
 if 'button_resumen' not in st.session_state:
     st.session_state.button_resumen = False
 
@@ -29,14 +27,11 @@ id_asis = st.secrets['security']['OPENAI_API_ASSIS_CONVERSACION']
 
 openai_client = sp.init_openai()
 assis_venta = sp.get_assistant(openai_client, id_asis)
-st.session_state.thread_conversacion = sp.create_thread(openai_client)
 
-col1, col2, col3 , col4, col5, col6, col7, col8 , col9, col10 = st.columns(10)
-
-with col1:
-    if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_resumen):
+if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_resumen):
         if user_input.strip():  # Evita entradas vacías
-            respuesta = sp.process_data(openai_client, assis_venta.id, st.session_state.thread_conversacion.id, user_input)
+            thread = sp.create_thread(openai_client)
+            respuesta = sp.process_data(openai_client, assis_venta.id, thread.id, user_input)
             st.session_state.historial_conversacion.append(f"Tú: {user_input}" + '\n' + 'Asistente: '+ respuesta)
             st.session_state.input_key_resumen += 1  # Cambiar clave para limpiar input
             st.session_state.button_resumen = False
@@ -44,23 +39,10 @@ with col1:
         else:
             st.session_state.button_resumen = False
             st.rerun()  # Refrescar la app para borrar input
-with col2:
-    pass
-with col4:
-    pass
-with col5:
-    pass
-with col6 :
-    pass
-with col7:
-    pass
-with col8:
-    pass
-with col9 :
-    pass
-with col10 :
-    if st.button("Borrar"):
+
+if st.button("Borrar"):
         st.session_state.historial_conversacion = []
         st.rerun()  # Refrescar la app para borrar input
+
 
 
--- tools/asistente_ventas.py
+++ tools/asistente_ventas.py
@@ -12,8 +12,6 @@ if "historial_ventas" not in st.session_state:
     st.session_state.historial_ventas = []
 if "input_key" not in st.session_state:
     st.session_state.input_key = 0  # Clave dinámica para resetear input
-if "thread_ventas" not in st.session_state:
-    st.session_state.thread_ventas = ''  # Clave dinámica para resetear input
 if 'button_ventas' not in st.session_state:
     st.session_state.button_ventas = False
 
@@ -29,14 +27,11 @@ id_asis = st.secrets['security']['OPENAI_API_ASSIS_VENTA']
 
 openai_client = sp.init_openai()
 assis_venta = sp.get_assistant(openai_client, id_asis)
-st.session_state.thread_ventas = sp.create_thread(openai_client)
 
-col1, col2, col3 , col4, col5, col6, col7, col8 , col9, col10 = st.columns(10)
-
-with col1:
-    if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_ventas):
+if st.button("Enviar" , on_click=click_button_send, disabled=st.session_state.button_ventas):
         if user_input.strip():  # Evita entradas vacías
-            respuesta = sp.process_data(openai_client, assis_venta.id, st.session_state.thread_ventas.id, user_input)
+            thread = sp.create_thread(openai_client)
+            respuesta = sp.process_data(openai_client, assis_venta.id, thread.id, user_input)
             st.session_state.historial_ventas.append(f"Tú: {user_input}" + '\n' + 'Asistente: '+ respuesta)
             st.session_state.input_key += 1  # Cambiar clave para limpiar input
             st.session_state.button_ventas = False
@@ -44,23 +39,10 @@ with col1:
         else:
             st.session_state.button_ventas = False
             st.rerun()  # Refrescar la app para borrar input
-with col2:
-    pass
-with col4:
-    pass
-with col5:
-    pass
-with col6 :
-    pass
-with col7:
-    pass
-with col8:
-    pass
-with col9 :
-    pass
-with col10 :
-    if st.button("Borrar"):
+if st.button("Borrar"):
         st.session_state.historial_ventas = []
         st.rerun()  # Refrescar la app para borrar input
+
+
 
 
